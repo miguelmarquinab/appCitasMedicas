1473e59e1db8c5549414955cddcd9ca1fecd5b19
Miguel Marquina <mmarquina@ue003cofopri.gob.pe>
2026-02-06 10:17:01 -0500

    base

---
+++ app/src/main/java/com/example/appcitasmedicas/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.appcitasmedicas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.appcitasmedicas.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.txtTitulo.text = "Login - AppCitasMedicas"
+//        enableEdgeToEdge()
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+}
